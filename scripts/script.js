f859a21c311c87714f2aa759d62fac8126bc8d93
Artem <artm.koshurnikov00@mail.ru>
2024-06-17 23:51:32 +0300

    /

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,12 @@
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext('2d');
+function createSquare(){
+    const square = document.createElement('div');
+    square.className = "square";
+    square.style.backgroundColor = 'red';
+    square.style.width = '100px';   
+    square.style.height = '100px'; 
+    document.body.appendChild(square);
+}
 
-// Получаем ширину и высоту холста
-const canvasWidth = canvas.width;
-const canvasHeight = canvas.height;
 
 
-
-// Рисование квадрата по центру
-ctx.fillStyle = 'red'; // Цвет заливки
-
-// Вычисляем координаты центра
-const squareX = canvasWidth / 2 - canvasWidth/2; // Центр по горизонтали минус половина ширины квадрата
-const squareY = canvasHeight / 2 - canvasHeight/2; // Центр по вертикали минус половина высоты квадрата
-
-ctx.fillRect(squareX, squareY, canvasWidth, canvasHeight); // (x, y, ширина, высота) 
 
